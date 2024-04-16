5c48797c830b267782b87f6be83a21f4b0beb754
myusername <148369766+Abhirn007@users.noreply.github.com>
2024-04-16 14:22:02 +0530

    grocery

---
+++ src/scripts/checkout.js
@@ -0,0 +1,6 @@
+document.querySelector('#place-order').addEventListener('click', function () {
+  localStorage.removeItem('cart')
+
+  document.getElementById('order-message').innerText =
+    'Your order is successfully placed'
+})
